52dedd41f88a7f9abbdc314294f76121245b5c9d
LowellC86 <lowellcaraballo@gmail.com>
2023-07-23 17:58:02 -0400

    portfolio done

--- portfolio_project/src/components/Projects.jsx
+++ portfolio_project/src/components/Projects.jsx
@@ -5,6 +5,7 @@ const Projects = () => {
     <div>
       <h2>Projects</h2>
       <a href="https://walksafenyc.netlify.app/">WalkSafeNYC</a>
+      <p>The app allows users to report incidents of street harassment in New York City (NYC) by entering an address or using their current location.</p>
     </div>
   );
 };
